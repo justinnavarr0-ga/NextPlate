b5969c45673de13196a61173e3f143984a00e15d
justinnavarr0-ga <justinnavarr0@yahoo.com>
2023-05-08 10:02:38 -0700

    Home and about page created

--- NextPlate/main_app/urls.py
+++ NextPlate/main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('recipes/', views.recipes_index, name='index')
+  path('about/', views.about, name='about'),
+  path('recipes/', views.recipes_index, name='index'),
 ]
--- NextPlate/main_app/views.py
+++ NextPlate/main_app/views.py
@@ -6,6 +6,9 @@ def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
 
+def about(request):
+  return render(request, 'about.html')
+
 def recipes_index(request):
     recipes = Recipe.objects.filter(user=request.user)
     return render(request, 'recipes/index.html', {
